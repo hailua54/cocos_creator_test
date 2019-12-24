4d3be34b5f554314533a5adc6babd923b4c4e7df
vu anh <hailua54@gmail.com>
2019-12-24 14:30:39 +0700

    m

--- assets/Script/HelloWorld.js
+++ assets/Script/HelloWorld.js
@@ -15,7 +15,7 @@ cc.Class({
 
     // use this for initialization
     onLoad: function () {
-        cc.log('Hello World! =================');
+        cc.log('Hello World!!! =================');
         this.label.string = this.text;
         vutils.setupDebugClick(this.node);
     },
@@ -23,11 +23,29 @@ cc.Class({
     onCocosClick(e)
     {
         cc.log('cocos click.');
-        var anim = this.coin.getComponent(cc.Animation);
-        var animState = anim.play('coin_anim');
+        //var anim = this.coin.getComponent(cc.Animation);
+        //var animState = anim.play('coin_anim');
         //var animState = anim.play('coin_anim_slow');
         //animState.wrapMode = cc.WrapMode.Loop;
-        var spine = this.coin.getComponent(sp.Skeleton);
+        //var spine = this.coin.getComponent(sp.Skeleton);
+
+        var view = this.node;
+        var coin = view.getChildByName('coin');
+        var way = 2;
+        if (way == 1)
+        {
+            var sp = cc.instantiate(coin);
+        }
+        else { // 2
+            var sp = new cc.Node();
+
+            let spriteComponent = sp.addComponent('cc.Sprite');
+            spriteComponent.spriteFrame = coin.getComponent('cc.Sprite').spriteFrame;
+        }
+        
+        sp.x = Math.random()*300;
+        sp.y = Math.random()*300;
+        this.node.addChild(sp);
     },
     // called every frame
     update: function (dt) {
