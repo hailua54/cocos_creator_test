429fe0aa9d5cd5196fb220f1d29ba5c2ca9fec7f
vu anh <hailua54@gmail.com>
2020-01-06 22:57:34 +0700

    update for onDestroy()

--- assets/Script/HelloWorld.js
+++ assets/Script/HelloWorld.js
@@ -14,6 +14,10 @@ cc.Class({
     },
     isMouseMoving: false,
     startTouchPos: null,
+    debugLabel: cc.Label,
+    fpsDur: 0,
+    fpsCount: 0,
+    debugStr: '',
     // use this for initialization
     onLoad: function () {
         cc.sys.dump();
@@ -50,7 +54,11 @@ cc.Class({
         var fruitSpine = this.node.getChildByName('fruitSpine');
         var freeSpineSpine = this.node.getChildByName('freeSpineSpine');
 
-        var t = 1;
+        //cc.view.enableAntiAlias(false);
+        this.debugLabel = this.node.getChildByName('label').getComponent(cc.Label);
+        this.fpsDur = 0;
+        this.fpsCount = 0;
+        this.debugStr = '123456 ';
     },
 
     updateGameSize()
@@ -59,6 +67,7 @@ cc.Class({
         var canvasComp = canvas.getComponent('cc.Canvas');
         var designSize = canvasComp.designResolution;
         var frameSize = cc.view.getFrameSize();
+        /*
         if (designSize.width / designSize.height > frameSize.width / frameSize.height)
         {
             canvasComp.fitWidth = false;
@@ -68,6 +77,8 @@ cc.Class({
             canvasComp.fitWidth = true;
             canvasComp.fitHeight = false;
         }
+        */
+        canvasComp.fitWidth = true;
     },
 
     onCocosClick(e)
@@ -82,6 +93,9 @@ cc.Class({
         var view = this.node;
         var coin = view.getChildByName('coin');
         var fruitSpine = view.getChildByName('fruitSpine');
+        var particleCoin = view.getChildByName('ParticleCoin');
+        //particleCoin.active = !particleCoin.active;
+        particleCoin.opacity = particleCoin.opacity ? 0 : 255;
         var way = 1;
         if (way == 1)
         {
@@ -97,22 +111,34 @@ cc.Class({
         sp.x = Math.random()*300;
         sp.y = Math.random()*300;
         this.node.addChild(sp);
+
+        var player = this.node.getComponent('SoundPlayer');
+        var r = cc.audioEngine.play(player.click);
     },
 
     onLoadSceneClick(e)
     {
-        this.node.parent.removeChild(this.node);
-        //cc.loader.release('29b52a20-83e9-4c69-ac7b-56d657db64ad');
-        cc.loader.releaseAll();
-        /*
-        cc.director.loadScene('GameScene', ()=>{
+        // IMPORTANCE NOTE: ----------------------------------------------------
+        // remove a node from scene can cause : the node and all it accessor not be able to call onDestroy()
+        // because the node is removed from tree graph call root_scope._deferredDestroy() -> accessor._destroyImmediate(){all childs + all components -> _destroyImmediate()}
+        //this.node.parent.removeChild(this.node, true);
+        //cc.loader.releaseAll();
+        // ----------------------------------------------------------------------
+        this.node.active = false;
+        cc.director.loadScene('helloworld', ()=>{
             cc.log('load GameScene complete!');
         });
-        */
     },
 
     // called every frame
     update: function (dt) {
-
+        this.fpsDur += dt;
+        this.fpsCount++;
+        if (this.fpsDur > 1)
+        {
+            this.fpsDur -= 1;
+            this.debugLabel.string = this.debugStr + ' ' + this.fpsCount;
+            this.fpsCount = 0;
+        }
     },
 });
